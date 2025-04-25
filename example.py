65051dc7781a05c27263aae73370ebb2dbb69eb3
John Raines <johndanielraines@gmail.com>
2025-04-24 22:44:11 -0500

    original implementation

---
+++ example.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+from pyclap import parser
+
+
+@parser
+class ExampleModel(BaseModel):
+    """
+    Example model for demonstration.
+    """
+    name: str
+    age: int = Field(default=30)
+    hobby: str | None
+
+
+if __name__ == "__main__":
+    args = ExampleModel.parse()
+    print(args)
+    for arg, val in vars(args).items():
+        print(f"{arg} of type {type(val).__name__}: {val}")
